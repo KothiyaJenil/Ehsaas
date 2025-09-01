5dfdb5c223305d26b478d62bda3d3a846f405b1c
KothiyaJenil <jenilkothiya.dev@gmail.com>
2025-09-01 21:32:48 +0530

    commit within the about section but navbar layout is panding

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('#menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  console.log('clicked');
+
+})
+
+
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 80);
+});
