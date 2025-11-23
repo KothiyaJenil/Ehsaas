2456071766d9a630875d732654c88ad6c897f184
KothiyaJenil <jenilkothiya.dev@gmail.com>
2025-11-23 17:08:59 +0530

    complete navbar

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+/* ----------------------------- navbar section ----------------------------- */
+const menuBtn = document.querySelector('#menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  console.log('clicked');
+
+})
